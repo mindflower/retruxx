d86d1a73f6ebb2141d9d1e063bd82711b44c9fe2
Alexander Belobrov <belobrov.ibks@gmail.com>
2024-08-27 12:58:26 +0300

    move common part to separate static lib

---
+++ retruxx_dll/dllmain.cpp
@@ -0,0 +1,29 @@
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+
+HINSTANCE g_hInst;
+DWORD	  g_amPlatform;		// VER_PLATFORM_WIN32_WINDOWS etc... (from GetVersionEx)
+OSVERSIONINFO g_osInfo;
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD reasonForCall,
+                      LPVOID
+)
+{
+    switch (reasonForCall)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, "Dll injected!", "retruxx", MB_OK);
+        break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+__declspec(dllexport) void __cdecl Function1(void)
+{
+}
