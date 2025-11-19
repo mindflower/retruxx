bd53139f85fada4c09d60c2b4cc298f1ebf79620
mindflower <belobrov.ibks@gmail.com>
2025-11-19 15:41:46 +0300

    objcontainer fix

--- retruxx_lib/src/server/colliders/vehiclecolliders.cpp
+++ retruxx_lib/src/server/colliders/vehiclecolliders.cpp
@@ -25,7 +25,9 @@ namespace ai
 
 	int CollideVehicleAndStatics(m3d::Object*, m3d::Object*, dContact*, unsigned&, bool)
 	{
-		RETRUXX_NOT_IMPLEMENTED;
+        // TODO: implement CollideVehicleAndLandscape
+        // RETRUXX_NOT_IMPLEMENTED;
+        return 1;
 	}
 
 	int CollideVehicleAndRoad(m3d::Object*, m3d::Object*, dContact*, unsigned&, bool)
